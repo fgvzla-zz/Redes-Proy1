bdb3ad2a46ea9f0f2583cd53f33e5b47289e883b
Franco Nori <francong2@gmail.com>
2013-11-18 21:34:11 -0430

    una de proyecto x

---
+++ cchat.h
@@ -0,0 +1,13 @@
+// Proyecto 1 - Cliente
+// Francisco Gamundi
+// Franco Nori
+ 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(int argc, char const *argv[])
+
+int conexion(int puerto, char* host)
