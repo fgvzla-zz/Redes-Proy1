da625d71adbcf339491fde36c48ed1d6e64cfc40
Francisco Gamundi <gamundiaranda@gmail.com>
2013-11-18 19:44:59 -0430

    Agregando archivos bases

---
+++ cchat.c
@@ -0,0 +1,12 @@
+// Proyecto 1 - Cliente
+// Francisco Gamundi
+// Franco Nori
+ 
+#include <sys/types.h>
+#include <sys/socket.h>
+ 
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
---
+++ schat.c
@@ -0,0 +1,12 @@
+// Proyecto 1 - Servidor
+// Francisco Gamundi
+// Franco Nori
+ 
+#include <sys/types.h>
+#include <sys/socket.h>
+ 
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
